b3c5053789aa2641cf5541146130295cca1a52a1
IEC\IEC920391 <tung.skelly@inventec.com>
2023-05-15 18:42:12 +0800

    修改: onnx轉檔程式碼, 驗證成功

--- SpeechRecogOK.py
+++ SpeechRecogOK.py
@@ -2,6 +2,8 @@ import pyaudio
 import wave
 import speech_recognition as sr
 import os
+import numpy as np
+
 
 # 獲取當前檔案的路徑 test
 # 在程式碼的開頭使用__file__變數來獲取當前檔案的路徑，然後搭配os.path模組中的函數來處理路徑，以獲取"audio.wav"和"model.onnx"的完整路徑。
@@ -69,3 +71,64 @@ except sr.UnknownValueError:
     print("無法識別音頻文件中的語音。")
 except sr.RequestError as e:
     print("無法從Google API獲取结果； {0}".format(e))
+
+
+# 將Python程式碼轉換為ONNX模型檔案
+import onnx
+from onnx import helper, TensorProto, numpy_helper
+
+# ...
+
+# 將Python程式碼轉換為ONNX模型檔案
+
+# 創建計算圖
+graph = helper.make_graph(
+    nodes=[],
+    name="speech_recognition",
+    inputs=[],
+    outputs=[],
+    initializer=[],
+)
+
+# 輸入
+input_name = "input"
+input_shape = (1, 16000)
+input_type = onnx.TensorProto.FLOAT
+
+input_tensor = helper.make_tensor_value_info(
+    name=input_name,
+    elem_type=input_type,
+    shape=input_shape
+)
+
+graph.input.extend([input_tensor])
+
+# 輸出
+output_name = "output"
+output_shape = (1,)
+output_type = onnx.TensorProto.STRING
+
+output_tensor = helper.make_tensor_value_info(
+    name=output_name,
+    elem_type=output_type,
+    shape=output_shape
+)
+
+graph.output.extend([output_tensor])
+
+# 初始化器
+initializer = [
+    numpy_helper.from_array(np.random.randn(3, 3, 1, 32).astype(np.float32), name='weight'),
+    numpy_helper.from_array(np.zeros(32, dtype=np.float32), name='bias')
+]
+
+graph.initializer.extend(initializer)
+
+# 創建ONNX模型
+model = helper.make_model(graph, producer_name='speech_recognition')
+
+# 將模型保存為ONNX檔案
+onnx_file_path = os.path.join(current_path, "speech_recognition.onnx")
+onnx.save_model(model, onnx_file_path)
+
+print("Conversion to ONNX is completed. The ONNX model is saved at: " + onnx_file_path)
